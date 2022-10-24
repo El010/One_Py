4cb8c7416e479c9312c2b159502e5814da338855
Vitor <vtga559@outlook.com>
2022-10-23 22:35:22 -0300

    Atualização de um programa

--- Curso em vídeo/Contação.py
+++ Curso em vídeo/Contação.py
@@ -1,5 +1,10 @@
 # Faça um algoritmo que leia quantos a pessoa tem em real e veja quantos ela vai ter em dolares
-a = float(input('Quantos você tem em real:\n '))
-b = a * 5.10
-print('Você tem R${} convertido em dollars sera {}\n'.format(a, b))
-
+a = float(input('Quantos você tem para viajar pelas americas\n '))
+b = a * 5.16
+c = a * 0.16
+d = a * 1.41
+e = a * 0.34
+print('Você tem R${} convertido em dollars R${} na cotação do dia 23/10/2022\n'.format(a, b))
+print('Real perante o peso bolivianon R${}\n'.format(c))
+print('emirirados árabes R${}\n'.format(d))
+print('Real diante do peso argentino R${}\n'.format(e, int(e)))
---
+++ Curso em vídeo/emoji.py
@@ -0,0 +1,3 @@
+import emoji
+print(emoji.emojize('python is :polegar_para_cima:',language='pt'))
+#print(emoji.emojize('python é :thumbs_up:'))
