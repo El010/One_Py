a471d8629deb2586537405e451cd56efe3719d42
Vitor <vtga559@outlook.com>
2022-11-07 19:47:04 -0300

    Algumas lições hoje

--- Curso em vídeo/ex-009.py
+++ Curso em vídeo/ex-009.py
@@ -8,5 +8,9 @@ print(fs)'''
 '''frase = 'curso em video de python'
 dividido = frase.split()
 print(dividido[2][3])''' # pega o Dividido 2 que e video e me mostre quem está na terceira posição sainda 'e'
+a = 'hello'
+print(a.find('ll'))
+b = input('nome: ')
+c = b.split()
+print(c, c[2]) # lista 
 
- 
--- Curso em vídeo/ex-022.py
+++ Curso em vídeo/ex-022.py
@@ -10,8 +10,23 @@
 02- colocar atributo 01 ao nome e printar
 03- adicionar todos os atributos ao nome e printando um a um.
 '''
-a = str(input('Digite seu nome completo: '))
+'''a = str(input('Digite seu nome completo: '))
 nome = a.upper()
 num =  a.lower()
-nu = a.
-print(nome, num, nu)
+nu = len(a)
+print(nome, num, nu)'''
+       #Resolução da aula 
+first=str(input('Nome completo: ')).strip()# vai puxar o nome e faze a contagens sem espaços 
+print('Analisando seu nome...')
+print('Seu nome de forma maiuscula é {}'.format(first.upper()))
+print('DE forma minuscula é {}'.format(first.lower()))
+print('Seu nome possue {} letras'.format(len(first)- first.count(' ')))# Ele vai ler as linhas e tirar os espaços me dando o valor
+# Sem os espaços 
+print('Seu primeiro nome tem {} letras'.format(first.find(' '))) # 'Find' vai encontrar o primeiro espaço e tudo que estiver antes do
+# espaço a funçao ira me retornar...
+recebe = first.split()
+print('Seu primeiro nome é {} ele tem {} letras'.format(recebe[0], len(recebe[0])))
+# Da segunda forma e criado mas uma variavel, dentro dela vai o split (que vai criar uma lista)
+# já na formataçao, 'recebe' eu pesso o paramentro 0 da lista, no caso o primeiro nome 
+# depois eu 'len'vai ler o nome é, vai me passar quantas letras tem no parameteo 0, todos os nomes que tem dentro. 
+
