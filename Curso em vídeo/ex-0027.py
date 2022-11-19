c96ea3776f08e1865af410f9cd8be96910613dbf
Vitor <vtga559@outlook.com>
2022-11-19 02:52:00 -0300

    primeiro e ultimo
    Pega o primeiro e último nome

--- Curso em vídeo/ex-0027.py
+++ Curso em vídeo/ex-0027.py
@@ -5,5 +5,7 @@ ex: ana maria de sousa
 1- ana
 2- sousa  
 '''
-nome = str(input('Digite seu nome completo: '))
-print('Seu nome separadamente é {}'.format(nome.split()))
+nome = str(input('Digite seu nome completo: ')).strip()
+firt = nome.split()
+print('Seu primeiro nome é {}'.format(firt[0]))
+print('Seu segundo nome é {}'.format(firt[len(firt)-1]))# len vai ler o nome e printar o ultimo nome que o usuario digitar
--- Curso em vídeo/ex-009.py
+++ Curso em vídeo/ex-009.py
@@ -10,6 +10,7 @@ dividido = frase.split()
 print(dividido[2][3])''' # pega o Dividido 2 que e video e me mostre quem está na terceira posição sainda 'e'
 a = 'hello'
 print(a.find('ll'))
+
 b = input('nome: ')
 c = b.split()
 print(c, c[2]) # lista 
