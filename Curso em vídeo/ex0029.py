37b1bc705016eb0ef6efd32f6e056262e6110887
Vitor <vtga559@outlook.com>
2022-11-25 23:48:15 -0300

    algoritmos
    Alguns algoritmos que recebe o valor, e passa formatada para o cliente usando if else

--- Curso em vídeo/ex0029.py
+++ Curso em vídeo/ex0029.py
@@ -6,7 +6,7 @@ A MULTA CUSTARA R$7,00 REAIS, POR CADA KM ACIMA DO LIMITE
 am=float(input('A quantos km você está? '))
 if am >= 80:
     print('você utrapassou limite')
-    print('valor da multa {:.2f}'.format())
+    print('valor da multa {:.2f}'.format((am-80)*7))
 else:
     print('tudo certo')
 
--- Curso em vídeo/ex0031.py
+++ Curso em vídeo/ex0031.py
@@ -2,3 +2,10 @@
 ESCREVA UM PROGRAMA QUE LEIA A DISTÃNCIA DE UM USUARIO EM KM,CALCULE O PREÇO DA PASSAGEM COBRANDO
 R$0,50 POR KM PARA VIAGENS DE ATÉ 200KM E R$0,45 PARA VIAGENS MAIS LONGAS 
 ''' 
+am = float(input('Quantos km você rodou?:\n'))
+if am <= 200:
+    print('O valor a ser pago sera...')
+    print('{}'.format(am*0.50))   
+else:
+    print('O valor da passagem sera...')
+    print('{}'.format(am*0.45))
