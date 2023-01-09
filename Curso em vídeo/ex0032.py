ba9ff676958c07636c51495503109f8d78ea26a2
Vitor <vtga559@outlook.com>
2023-01-09 13:13:41 -0300

    Ano_bissexto
    Algoritmo que ler um ano qualquer, e mostra se ele e bissexto ou não

--- Curso em vídeo/ex0032.py
+++ Curso em vídeo/ex0032.py
@@ -1,3 +1,9 @@
 '''
 FAÇA UM PROGRAMA QUE LEIA UM ANO QUALQUER E MOSTRE SE ELE E BISSEXTO
 '''
+a=int(input('Entre com um numero de ano: '))
+if ( a % 4 == 0  and a % 100!=0) or (a%400==0):
+    print('bissexto')
+else:
+    print('Não bissexto')
+# and a%100!=0) or (a%400==0):
